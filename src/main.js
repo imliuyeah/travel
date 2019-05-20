89511900f62f9c0ba3d5be10991e3a97f881be50
imliuyeah <imliuyeah@gmail.com>
2019-05-20 22:55:38 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 在终端中输入 npm install fastclick --save 后下载fastClick引入，解决使用click事件会延迟300毫秒才执行的问题
+import fastClick from 'fastClick'
+import './assets/styles/reset.css'
+// 解决移动端1像素边框被显示为多像素的bug的css文件
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<!--    由于template只能向外暴露一个根标签，所以要在最外层包裹一个标签
+	<div>
+    <div>home</div>
+    在Vue中，一般用router-link实现跳转，点击链接跳转到当前目录下的list页面，和a标签效果相似
+    <router-link to="/list" class="home">列表页</router-link>
+  </div> -->
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
