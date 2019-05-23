ac2f14590d111673474e89181e65dadc84641d53
imliuyeah <imliuyeah@gmail.com>
2019-05-23 11:37:03 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// 在终端中输入 npm install fastclick --save 后下载fastClick引入，解决使用click事件会延迟300毫秒才执行的问题
 import fastClick from 'fastClick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
-// 解决移动端1像素边框被显示为多像素的bug的css文件
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,42 +1,55 @@
 <template>
-  <div class="header">
-    <div class="header-left">左边</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
-  </div>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe658;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe63c;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市<span class="iconfont">&#xe65c;</span>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'header'
+    name: 'Header'
 }
 </script>
 
-<style>
-	.header {
-    display: flex;
-		line-height: .86rem;
-		background: #00bcd4;
-    color: #fff;
-	}
-  .header-left {
-    float: left;
-    width: .64rem;
-  }
-  .header-input{
-    flex: 1;
-    height: .64rem;
-    margin-top: .12rem;
-    margin-left: .2rem;
-    border-radius: .1rem;
-    line-height: .64rem;
-    background: #fff;
-    color: #ccc;
+<style lang="stylus" scoped>
 
-  }
-  .header-right {
-    float: right;
-    width: 1.24rem;
-    text-align: center;
-  }
+    @import '../../../assets/styles/varibles.styl'
+    .header {
+        display: flex;
+        line-height: .86rem;
+        background: $bgColor;
+        color: #fff;
+    }
+    .header-left {
+        float: left;
+        width: .64rem;
+    }
+    .back-icon {
+        text-align: center;
+        font-size: .5rem;
+    }
+    .header-input {
+        flex: 1;
+        height: .64rem;
+        margin-top: .12rem;
+        margin-left: .2rem;
+        padding-left: .2rem;
+        border-radius: .1rem;
+        line-height: .64rem;
+        background: #fff;
+        color: #ccc;
+    }
+    .header-right {
+        float: right;
+        width: 1.24rem;
+        text-align: center;
+    }
 </style>
