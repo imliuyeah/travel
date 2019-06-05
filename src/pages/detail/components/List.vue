7e380ed241f4a0260d5b5a4121f382fc98e162fb
imliuyeah <imliuyeah@gmail.com>
2019-06-05 11:47:27 +0800

    detail list finish

--- src/pages/detail/components/List.vue
+++ src/pages/detail/components/List.vue
@@ -6,7 +6,7 @@
                 {{item.title}}
             </div>
             <div v-if="item.children" class="item-children">
-                <detail-List :list="item.children"></detail-List>
+                <detail-list :list="item.children"></detail-list>
             </div>
         </div>
     </div>
