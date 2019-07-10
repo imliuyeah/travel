7995408d9ec0e889047a1d6ea83c862c1ceb538e
imliuyeah <imliuyeah@gmail.com>
2019-07-10 15:38:41 +0800

    完成详情页announce区域

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,68 +1,73 @@
 <template>
-    <div>
-        <detail-banner
-         :bannerImg = "bannerImg"
-         :gallaryImgs = "gallaryImgs"
-         :sightName = "sightName"
-        ></detail-banner>
-        <detail-header></detail-header>
-        <detail-info></detail-info>
-        <detail-List :list="list"></detail-List>
-        <div class="container"></div>
-    </div>
+  <div class="wrapper">
+    <detail-banner :bannerImg="bannerImg" :gallaryImgs="gallaryImgs" :sightName="sightName"></detail-banner>
+    <detail-header></detail-header>
+    <detail-info></detail-info>
+    <detail-announce></detail-announce>
+    <detail-List :list="list"></detail-List>
+    <div class="container"></div>
+  </div>
 </template>
 
 <script>
-import DetailBanner from './components/Banner'
-import DetailHeader from './components/Header'
-import DetailInfo from './components/Info'
-import DetailList from './components/List'
-import axios from 'axios'
+import DetailBanner from "./components/Banner";
+import DetailHeader from "./components/Header";
+import DetailInfo from "./components/Info";
+import DetailAnnounce from "./components/Announce";
+import DetailList from "./components/List";
+import axios from "axios";
 
 export default {
-    name: 'Detail',
-    components: {
-        DetailBanner,
-        DetailHeader,
-        DetailInfo,
-        DetailList
+  name: "Detail",
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailInfo,
+    DetailAnnounce,
+    DetailList
+  },
+  data() {
+    return {
+      bannerImg: "",
+      list: [],
+      gallaryImgs: [],
+      sightName: ""
+    };
+  },
+  methods: {
+    getHomeInfo() {
+      // 这里等于 '/api/detail.json?id=' + this.$route.params
+      axios
+        .get("/api/detail.json", {
+          params: {
+            id: this.$route.params.id
+          }
+        })
+        .then(this.getHomeInfoSucc);
     },
-    data () {
-        return {
-            bannerImg: '',
-            list: [],
-            gallaryImgs: [],
-            sightName: ''
-        }
-    },
-    methods: {
-        getHomeInfo () {
-            // 这里等于 '/api/detail.json?id=' + this.$route.params
-            axios.get('/api/detail.json', {
-               params: {
-                   id: this.$route.params.id
-               } 
-            }).then(this.getHomeInfoSucc)
-        },
-        getHomeInfoSucc (res) {
-            res = res.data
-            if( res.ret && res.data) {
-                const data = res.data
-                this.bannerImg = data.bannerImg
-                this.list = data.categoryList
-                this.gallaryImgs = data.gallaryImgs
-                this.sightName = data.sightName
-            }
-        }
-    },
-    mounted () {
-        this.getHomeInfo()
+    getHomeInfoSucc(res) {
+      res = res.data;
+      if (res.ret && res.data) {
+        const data = res.data;
+        this.bannerImg = data.bannerImg;
+        this.list = data.categoryList;
+        this.gallaryImgs = data.gallaryImgs;
+        this.sightName = data.sightName;
+      }
     }
-}
+  },
+  mounted() {
+    this.getHomeInfo();
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-    .container {
-        height 50rem;
-    }
+@import '~styles/varibles.styl'
+.wrapper
+  background: $detailbcg
+.container
+  background: #fff
+  height: 50rem
+
 </style>
